396672d8043adf295079276d5f81af189309aecb
Vladislav Bogdan <vladislav.bogdan97@gmail.com>
2019-05-19 21:32:06 +0200

    google login

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,41 @@
+var userCredentials = {
+    KEY_PREVIOUS_LOGIN : "key_previous_login",
+    VALUE_FACEBOOK : "facebook",
+    VALUE_GOOGLE : "google",
+    isValid : function(){
+        return this._firebaseToken != 'undefined' &&
+            this._userEmail != 'undefined';
+    },
+    checkPreviousFacebookLogin : function(){
+        if (localStorage.getItem(KEY_PREVIOUS_LOGIN) === null) {
+            return false;
+        }
+        return localStorage.getItem(KEY_PREVIOUS_LOGIN) == VALUE_FACEBOOK;
+    },
+    checkPreviousGoogleLogin : function(){
+        if (localStorage.getItem(KEY_PREVIOUS_LOGIN) === null) {
+            return false;
+        }
+        return localStorage.getItem(KEY_PREVIOUS_LOGIN) == VALUE_GOOGLE;
+    },
+    setGoogleAuth(email){
+        saveLoginSource(VALUE_GOOGLE);
+        setUserEmail(email);
+    },
+    setFacebookAuth(email) {
+        saveLoginSource(VALUE_FACEBOOK);
+        setUserEmail(email);
+    },
+    setUserEmail : function(email){
+        this._userEmail = email;
+    },
+    setToken : function(token){
+        this._firebaseToken = token;
+    },
+    saveLoginSource : function(source){
+        window.localStorage.setItem(KEY_PREVIOUS_LOGIN, source);
+    }
+}; // User credentials required to login, _firebaseToken, _userEmail
 
 var app = {
     initialize: function() {
@@ -10,48 +48,8 @@ var app = {
         document.addEventListener('resume', this.onResume, false);
     },
     onDeviceReady: function() {
-
-
-/*
-        const options = {
-          method: 'get',
-        };
-
-        cordova.plugin.http.sendRequest('http://dolphinpaysv.us-west-2.elasticbeanstalk.com:80/rest_api/v1/states', options, function(response) {
-          // prints 200
-          console.log(response.data);
-        }, function(response) {
-          // prints 403
-          console.log(response.status);
-
-          //prints Permission denied
-          console.log(response.error);
-        });
-
-        window.FirebasePlugin.getToken(function(token) {
-            // save this server-side and use it to push notifications to this device
-            console.log(token);
-        }, function(error) {
-            console.error(error);
-        });
-
-        // Get notified when a token is refreshed
-        window.FirebasePlugin.onTokenRefresh(function(token) {
-            // save this server-side and use it to push notifications to this device
-            console.log("Refresh to get new token: " + token);
-        }, function(error) {
-            alert(error);
-        });
-
-        // Get notified when the user opens a notification
-        window.FirebasePlugin.onNotificationOpen(function(notification) {
-            console.log(JSON.stringify(notification));
-            alert("The notification is open!");
-        }, function(error) {
-            console.error(error);
-        });
-
-*/
+        _setLoadingUI();
+        requestFirebaseToken();
     },
     onPause: function() {
 
@@ -62,17 +60,88 @@ var app = {
 }
 app.initialize();
 
+/**
+*   Use to setting up Token refresh and GetToken listener
+*   Server auth requires firebase token and user email
+*/
+function requestFirebaseToken(){
+        window.FirebasePlugin.getToken(function(token) {
+            // save this server-side and use it to push notifications to this device
+            console.log(token);
+            userCredentials.setToken(token);
+            if(userCredentials.checkPreviousGoogleLogin()){
+                requestGoogleLoginSilent();
+            }else{
+                _setBaseLoginUI();
+            }
+        }, function(error) {
+            if(navigator.network.connection.type == Connection.NONE){
+                // TODO signal interner connection absent and set base ui
+            }else{
+                // TODO signal abort
+            }
+        });
+}
 
-function gpluslogin(){
-    window.plugins.googleplus.login(
+function requestGoogleLoginSilent(){
+    window.plugins.googleplus.trySilentLogin(
         {
+            'offline': false, // Optional, but requires the webClientId - if set to true the plugin will also return a serverAuthCode, which can be used to grant offline access to a non-Google server
         },
         function (obj) {
-          alert(JSON.stringify(obj)); // do something useful instead of alerting
+            userCredentials.setGoogleAuth(obj.email);
+            requestServerAuth();
         },
         function (msg) {
-          alert('error: ' + msg);
+            requestGoogleLoginExplicit();
         }
     );
+}
 
+function requestGoogleLoginExplicit(){
+    window.plugins.googleplus.login(
+        {
+            'offline': false, // optional, but requires the webClientId - if set to true the plugin will also return a serverAuthCode, which can be used to grant offline access to a non-Google server
+        },
+        function (obj) {
+            userCredentials.setGoogleAuth(obj.email);
+            requestServerAuth();
+        },
+        function (msg) {
+            if(navigator.network.connection.type == Connection.NONE){
+                // TODO signal interner connection absent and set base ui
+            }else{
+                // TODO signal abort
+            }
+        }
+    );
+}
+
+function requestFacebookLoginExplicit(){
+    // TODO coming soon
+}
+
+function requestServerAuth(){
+    if(!userCredentials.isValid()){
+        return; // Missing credentials
+    }
+
+    //todo fix me
+    window.location = "preview.html";
+}
+
+function _signalAbort(){
+    // TODO
+}
+
+function _signalRequestInternetConnection(_callback){
+    // TODO
+}
+
+function _setBaseLoginUI(){
+     // TODO
+}
+
+function _setLoadingUI(){
+    // TODO
 }
