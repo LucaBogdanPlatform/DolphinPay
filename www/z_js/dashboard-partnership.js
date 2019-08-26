3ba0e917899fde4f56b3e848ecf3da220fbeb56e
Vladislav Bogdan <vladislav.bogdan97@gmail.com>
2019-08-26 11:50:25 +0200

    Dashboard show user info

---
+++ www/z_js/dashboard-partnership.js
@@ -0,0 +1,28 @@
+var app = {
+    initialize: function() {
+        this.bindEvents();
+    },
+    bindEvents: function() {
+        // Here we register our callbacks for the lifecycle events we care about
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('pause', this.onPause, false);
+        document.addEventListener('resume', this.onResume, false);
+    },
+    onDeviceReady: function() {
+        attachBaseMenuListeners();
+        attachBaseLeftMenuListeners();
+    },
+
+    onPause: function() {
+
+    },
+    onResume: function(event) {
+
+    }
+//    onBackKeyDown: function(){
+//        alert("A");
+//        navigator.app.exitApp();
+//        return false;
+//    }
+}
+app.initialize();
